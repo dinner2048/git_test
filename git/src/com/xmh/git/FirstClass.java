16266f31b029864b514922601c8072fe223474ad
xuminghao <xuminghao067@136.com>
2021-01-21 17:24:24 +0800

    把本地工程添加到git

---
+++ git/src/com/xmh/git/FirstClass.java
@@ -0,0 +1,12 @@
+package com.xmh.git;
+
+/**
+ * @author dinner
+ * @description
+ * @create 2021/1/21
+ */
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("hello git project");
+    }
+}
