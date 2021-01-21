1d4b9755e3ccdfe8a580373a202334d2a506b76f
xuminghao <xuminghao067@136.com>
2021-01-21 17:26:17 +0800

    添加test方法

--- git/src/com/xmh/git/FirstClass.java
+++ git/src/com/xmh/git/FirstClass.java
@@ -9,4 +9,9 @@ public class FirstClass {
     public static void main(String[] args) {
         System.out.println("hello git project");
     }
+
+    public void test(){
+        int i = 0;
+        i += 10;
+    }
 }
