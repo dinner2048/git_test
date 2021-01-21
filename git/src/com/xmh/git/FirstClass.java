580cefecb2a9e650ba61661324029df3fc27ccaa
xuminghao <xuminghao067@136.com>
2021-01-21 17:42:45 +0800

    修改注释

--- git/src/com/xmh/git/FirstClass.java
+++ git/src/com/xmh/git/FirstClass.java
@@ -8,6 +8,7 @@ package com.xmh.git;
 public class FirstClass {
     public static void main(String[] args) {
         System.out.println("hello git project");
+        System.out.println("hello git project");
     }
 
     public void test(){
